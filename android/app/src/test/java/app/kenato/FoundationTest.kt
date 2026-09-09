f51b9e30c36da737b6d76590fc6bb56164f270d8
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-09 14:33:36 +0300

    m0: add android/app/src/test/java/app/kenato/FoundationTest.kt

---
+++ android/app/src/test/java/app/kenato/FoundationTest.kt
@@ -0,0 +1,11 @@
+package app.kenato
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class FoundationTest {
+    @Test
+    fun productNameIsStable() {
+        assertEquals("Kenato", "Kenato")
+    }
+}
