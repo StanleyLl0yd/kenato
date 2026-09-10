b5bfc732f61510de3c0e8ebebfcada8b024aeee4
Stanley Lloyd <131160519+StanleyLl0yd@users.noreply.github.com>
2026-09-10 16:43:23 +0300

    build: wire ZXing QR dependency

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -86,6 +86,7 @@ dependencies {
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.zxing.core)
 
     debugImplementation(libs.androidx.compose.ui.tooling)
     testImplementation(libs.junit4)
